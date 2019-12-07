696f4db1a2482b9f3f876722a0879f4472d8c6ba
Maciej Krüger <mkg20001@gmail.com>
2019-12-07 09:07:39 +0100

    all the shit

--- pkgs/desktops/cinnamon/default.nix
+++ pkgs/desktops/cinnamon/default.nix
@@ -1,11 +1,14 @@
 { pkgs, lib }:
 
 lib.makeScope pkgs.newScope (self: with self; {
+  cinnamon-common = callPackage ./cinnamon-common { };
   cinnamon-control-center = callPackage ./cinnamon-control-center { };
   cinnamon-desktop = callPackage ./cinnamon-desktop { };
   cinnamon-menus = callPackage ./cinnamon-menus { };
   cinnamon-translations = callPackage ./cinnamon-translations { };
+  cinnamon-session = callPackage ./cinnamon-session { };
   cinnamon-settings-daemon = callPackage ./cinnamon-settings-daemon { };
+  cinnamon-screensaver = callPackage ./cinnamon-screensaver { };
   cjs = callPackage ./cjs { };
   nemo = callPackage ./nemo { };
   muffin = callPackage ./muffin { };
