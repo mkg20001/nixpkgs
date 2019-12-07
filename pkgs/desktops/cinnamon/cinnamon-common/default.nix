bb5134fe2fe7b97a8cd3d3b9ebd93e17b061a1de
Maciej Krüger <mkg20001@gmail.com>
2019-12-07 09:07:39 +0100

    all the shit

---
+++ pkgs/desktops/cinnamon/cinnamon-common/default.nix
@@ -0,0 +1,94 @@
+{ atk,
+  autoreconfHook,
+  cacert,
+  cinnamon-desktop,
+  cinnamon-menus,
+  cjs,
+  dbus-glib,
+  fetchFromGitHub,
+  gdk-pixbuf,
+  glib,
+  gobject-introspection,
+  gtk3,
+  intltool,
+  json-glib,
+  libcroco,
+  libsoup,
+  libstartup_notification,
+  libXtst,
+  muffin,
+  networkmanager,
+  pkgconfig,
+  polkit,
+  stdenv,
+  wrapGAppsHook,
+  libxml2,
+  gnome2,
+  gnome3,
+  python3,
+  keybinder3,
+  cairo,
+  xapps,
+  upower }:
+
+stdenv.mkDerivation rec {
+  pname = "cinnamon-common";
+  version = "4.4.1";
+  enableParallelBuilding = true;
+
+  src = fetchFromGitHub {
+    owner = "linuxmint";
+    repo = "cinnamon";
+    rev = version;
+    sha256 = "0sv7nqd1l6c727qj30dcgdkvfh1wxpszpgmbdyh58ilmc8xklnqd";
+  };
+
+  buildInputs = [ # TODO: review if we really need this all
+    (python3.withPackages (pp: with pp; [ setproctitle pygobject3 pycairo ]))
+    atk
+    cacert
+    cinnamon-desktop
+    cinnamon-menus
+    cjs
+    dbus-glib
+    gdk-pixbuf
+    glib
+    gtk3
+    json-glib
+    libcroco
+    libsoup
+    libstartup_notification
+    libXtst muffin
+    networkmanager
+    pkgconfig
+    polkit
+    libxml2
+
+    # bindings
+    cairo
+    gnome3.caribou
+    keybinder3
+    upower
+    xapps
+  ];
+
+  nativeBuildInputs = [
+    autoreconfHook
+    wrapGAppsHook
+    intltool
+    gnome2.gtkdoc
+    gobject-introspection
+  ];
+
+  autoreconfPhase = ''
+    GTK_DOC_CHECK=false NOCONFIGURE=1 bash ./autogen.sh
+    '';
+
+  configureFlags = [ "--disable-static" "--with-ca-certificates=${cacert}/etc/ssl/certs/ca-bundle.crt" "--with-libxml=${libxml2.dev}/include/libxml2" "--enable-gtk-doc=no" ];
+
+  postPatch = ''
+    substituteInPlace src/Makefile.am \
+      --replace "\$(libdir)/muffin" "${muffin}/lib/muffin"
+    patchShebangs autogen.sh
+  '';
+}
