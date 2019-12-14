cc89b2ec54f3d0b1db3bb857ecd6369a1256362b
Maciej Krüger <mkg20001@gmail.com>
2019-12-15 00:43:16 +0100

    bless: init at 0.6.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1194,6 +1194,8 @@ in
 
   behdad-fonts = callPackage ../data/fonts/behdad-fonts { };
 
+  bless = callPackage ../applications/editors/bless { };
+
   blink1-tool = callPackage ../tools/misc/blink1-tool { };
 
   bliss = callPackage ../applications/science/math/bliss { };
