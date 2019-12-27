86cf7a6421d234a5e97b05265ed44428c87ae45e
Maciej Krüger <mkg20001@gmail.com>
2019-12-27 08:22:08 +0100

    Revert "tmp"
    This reverts commit 97e94503004471405dc6dd8c79853f300377cf73.

--- pkgs/applications/networking/sniffers/ettercap/default.nix
+++ pkgs/applications/networking/sniffers/ettercap/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, fetchpatch, cmake, libpcap, libnet, zlib, curl, pcre
+{ stdenv, fetchFromGitHub, cmake, libpcap, libnet, zlib, curl, pcre
 , openssl, ncurses, glib, gtk3, atk, pango, flex, bison, geoip, harfbuzz
 , pkgconfig }:
 
@@ -12,13 +12,6 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "0m40bmbrv9a8qlg54z3b5f8r541gl9vah5hm0bbqcgyyljpg39bz";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/Ettercap/ettercap/compare/ebc85131c656ea13907195b6e1981e0f8c121c50...78da46d1488fa9903344fc993555461f54ae9c6a.diff";
-      sha256 = "02nzps4ca3d20gvykyaz8qjnsgjvks2czfmq8ic9xk17v2pn19ji";
-    })
-  ];
 
   strictDeps = true;
   nativeBuildInputs = [ cmake flex bison pkgconfig ];
@@ -37,6 +30,8 @@ stdenv.mkDerivation rec {
     "-DBUNDLED_LIBS=Off"
     "-DGTK3_GLIBCONFIG_INCLUDE_DIR=${glib.out}/lib/glib-2.0/include"
   ];
+
+  NIX_CFLAGS_COMPILE = [ "-I${harfbuzz.dev}/include/harfbuzz" ];
 
   meta = with stdenv.lib; {
     description = "Comprehensive suite for man in the middle attacks";
