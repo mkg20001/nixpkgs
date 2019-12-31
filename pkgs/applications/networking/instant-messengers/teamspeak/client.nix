22f9ec374951a68bf70964dd74d91e518df90735
Maciej Krüger <mkg20001@gmail.com>
2019-12-31 01:32:05 +0100

    teamspeak_client: fix icon

--- pkgs/applications/networking/instant-messengers/teamspeak/client.nix
+++ pkgs/applications/networking/instant-messengers/teamspeak/client.nix
@@ -1,6 +1,7 @@
 { lib, stdenv, fetchurl, makeWrapper, makeDesktopItem, zlib, glib, libpng, freetype, openssl
 , xorg, fontconfig, qtbase, qtwebengine, qtwebchannel, qtsvg, qtwebsockets, xkeyboard_config
 , alsa-lib, libpulseaudio ? null, libredirect, quazip, which, unzip, llvmPackages, writeShellScriptBin
+, imagemagick
 }:
 
 let
@@ -48,7 +49,7 @@ stdenv.mkDerivation rec {
     sha256 = "1bywmdj54glzd0kffvr27r84n4dsd0pskkbmh59mllbxvj0qwy7f";
   };
 
-  nativeBuildInputs = [ makeWrapper fakeLess which unzip ];
+  nativeBuildInputs = [ makeWrapper fakeLess which unzip imagemagick ];
 
   unpackPhase =
     ''
@@ -83,7 +84,10 @@ stdenv.mkDerivation rec {
       # Make a desktop item
       mkdir -p $out/share/applications/ $out/share/icons/hicolor/64x64/apps/
       unzip ${pluginsdk}
-      cp pluginsdk/docs/client_html/images/logo.png $out/share/icons/hicolor/64x64/apps/teamspeak.png
+      convert pluginsdk/docs/client_html/images/logo.png -resize 48x48 48.png
+
+      install -D pluginsdk/docs/client_html/images/logo.png          $out/share/icons/hicolor/64x64/apps/teamspeak.png
+      install -D 48.png                                              $out/share/icons/hicolor/48x48/apps/teamspeak.png
       cp ${desktopItem}/share/applications/* $out/share/applications/
 
       # Make a symlink to the binary from bin.
