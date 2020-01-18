41e5045186b37e3d2969a77e9ffdde9ac57c7c8b
Maciej Krüger <mkg20001@gmail.com>
2020-01-18 14:52:43 +0100

    screensaver fix

--- pkgs/desktops/cinnamon/cinnamon-screensaver/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-screensaver/default.nix
@@ -27,6 +27,7 @@
 , accountsservice
 , cairo
 , xapps
+, cinnamon-desktop
 }:
 
 stdenv.mkDerivation rec {
@@ -49,6 +50,7 @@ stdenv.mkDerivation rec {
     pam
     accountsservice
     cairo
+    cinnamon-desktop
   ];
 
   NIX_CFLAGS_COMPILE = "-I${glib.dev}/include/gio-unix-2.0"; # TODO: https://github.com/NixOS/nixpkgs/issues/36468
