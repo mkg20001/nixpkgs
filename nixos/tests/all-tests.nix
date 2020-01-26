bcbccde68b6e140e7137ea44412f7f018ab86c7f
Maciej Krüger <mkg20001@gmail.com>
2020-01-26 17:21:36 +0100

    users-groups: add test for environment.skel option

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -188,6 +188,7 @@ in {
   enlightenment = handleTest ./enlightenment.nix {};
   env = handleTest ./env.nix {};
   envoy = handleTest ./envoy.nix {};
+  environmentSkel = callTest ./environment-skel.nix {};
   ergo = handleTest ./ergo.nix {};
   ergochat = handleTest ./ergochat.nix {};
   etc = pkgs.callPackage ../modules/system/etc/test.nix { inherit evalMinimalConfig; };
