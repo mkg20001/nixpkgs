7607c1e7ab4fd83b0bd21638da694c97dc6c6b46
Maciej Krüger <mkg20001@gmail.com>
2020-01-26 17:21:36 +0100

    users-groups: add test for environment.skel option

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -139,6 +139,7 @@ in
   engelsystem = handleTest ./engelsystem.nix {};
   enlightenment = handleTest ./enlightenment.nix {};
   env = handleTest ./env.nix {};
+  environmentSkel = callTest ./environment-skel.nix {};
   ergo = handleTest ./ergo.nix {};
   ergochat = handleTest ./ergochat.nix {};
   etcd = handleTestOn ["x86_64-linux"] ./etcd.nix {};
