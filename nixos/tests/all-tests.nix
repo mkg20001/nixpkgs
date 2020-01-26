9863ec2de38951740f3c372cf0eb2a62ce0ae17f
Maciej Krüger <mkg20001@gmail.com>
2020-01-26 17:21:36 +0100

    users-groups: add test for environment.skel option

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -284,6 +284,7 @@ in {
   env = handleTest ./env.nix {};
   envfs = handleTest ./envfs.nix {};
   envoy = handleTest ./envoy.nix {};
+  environmentSkel = callTest ./environment-skel.nix {};
   ergo = handleTest ./ergo.nix {};
   ergochat = handleTest ./ergochat.nix {};
   eris-server = handleTest ./eris-server.nix {};
