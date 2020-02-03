8e459ed5e70fe7c15492c0a31f233b2c99a46837
Maciej Krüger <mkg20001@gmail.com>
2020-02-03 15:21:42 +0100

    revertions

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1012,8 +1012,6 @@ in {
 
   piccata = callPackage ../development/python-modules/piccata {};
 
-  pick-colour-picker = callPackage ../development/python-modules/pick-colour-picker {};
-
   pims = callPackage ../development/python-modules/pims { };
 
   poetry = callPackage ../development/python-modules/poetry { };
