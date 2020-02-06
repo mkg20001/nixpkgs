6408f8efbefcc0c82af5663f938856579cb35aed
Maciej Krüger <mkg20001@gmail.com>
2020-02-06 03:24:13 +0100

    environment.aliases: add module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -138,6 +138,7 @@
   ./i18n/input-method/uim.nix
   ./image/images.nix
   ./installer/tools/tools.nix
+  ./misc/aliases.nix
   ./misc/assertions.nix
   ./misc/crashdump.nix
   ./misc/documentation.nix
