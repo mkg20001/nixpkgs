3c673e9cb658fc074ab4cbb3b8e35ff8c6de914d
Maciej Krüger <mkg20001@gmail.com>
2020-02-06 03:24:30 +0100

    environment.aliases: add test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -484,6 +484,7 @@ in
   enlightenment = runTest ./enlightenment.nix;
   env = runTest ./env.nix;
   envfs = runTest ./envfs.nix;
+  environmentAliases = callTest ./environment-aliases.nix {};
   envoy = runTest {
     imports = [ ./envoy.nix ];
     _module.args.envoyPackage = pkgs.envoy;
