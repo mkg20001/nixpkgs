98d6aa14de3e435d0b0b03e969ecf855eed9489e
Maciej Krüger <mkg20001@gmail.com>
2020-02-06 03:24:30 +0100

    environment.aliases: add test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -286,6 +286,7 @@ in {
   env = handleTest ./env.nix {};
   envfs = handleTest ./envfs.nix {};
   envoy = handleTest ./envoy.nix {};
+  environmentAliases = callTest ./environment-aliases.nix {};
   environmentSkel = callTest ./environment-skel.nix {};
   ergo = handleTest ./ergo.nix {};
   ergochat = handleTest ./ergochat.nix {};
