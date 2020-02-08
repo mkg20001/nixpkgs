628a980998e8a99f2eb6d2f33c7ab21ce771a2b2
Maciej Krüger <mkg20001@gmail.com>
2020-02-08 18:08:24 +0100

    environment.links: add test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -555,6 +555,7 @@ in
     _module.args.systemdStage1 = true;
   };
   environmentAliases = callTest ./environment-aliases.nix {};
+  environmentLinks = callTest ./environment-links.nix {};
   envoy = runTest {
     imports = [ ./envoy.nix ];
     _module.args.envoyPackage = pkgs.envoy;
