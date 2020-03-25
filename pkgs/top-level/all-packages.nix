75babe2c37445f58378545ecbb38322ac2c59e48
luc65r <lucas.r065@yahoo.fr>
2020-03-25 11:14:46 +0100

    mate-tweak: init at 20.04.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3373,6 +3373,8 @@ with pkgs;
 
   masscan = callPackage ../tools/security/masscan { };
 
+  mate-tweak = python3Packages.callPackage ../tools/misc/mate-tweak { };
+
   massren = callPackage ../tools/misc/massren { };
 
   mat2 = with python3.pkgs; toPythonApplication mat2;
