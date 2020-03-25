ff2d9a6fc3dc2314c6c58847ab3c807c4a920842
luc65r <lucas.r065@yahoo.fr>
2020-03-25 11:14:46 +0100

    mate-tweak: init at 20.04.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2083,6 +2083,8 @@ in
 
   marlin-calc = callPackage ../tools/misc/marlin-calc {};
 
+  mate-tweak = python3Packages.callPackage ../tools/misc/mate-tweak { };
+
   mathics = with python2Packages; toPythonApplication mathics;
 
   masscan = callPackage ../tools/security/masscan {
