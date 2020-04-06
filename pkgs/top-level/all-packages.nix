f992207d3879dd181a4b93297b0997ff17e1f0ba
Maciej Krüger <mkg20001@gmail.com>
2020-04-06 03:43:02 +0200

    Revert "libcroco: remove as it's no longer used"
    This reverts commit aef3860211c9b1475aa69cb49688fd6b0db206c1.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4615,6 +4615,8 @@ in
 
   libcoap = callPackage ../applications/networking/libcoap {};
 
+  libcroco = callPackage ../development/libraries/libcroco { };
+
   libcryptui = callPackage ../development/libraries/libcryptui { };
 
   libsmi = callPackage ../development/libraries/libsmi { };
