17ce4dd6962abebaf56fc32955e0c3c05d59d571
Maciej Krüger <mkg20001@gmail.com>
2020-04-22 14:38:58 +0200

    lbry: init at 0.44.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22206,6 +22206,8 @@ in
 
   liferea = callPackage ../applications/networking/newsreaders/liferea { };
 
+  lbry = callPackage ../applications/video/lbry { };
+
   lightworks = callPackage ../applications/video/lightworks {
     portaudio = portaudio2014;
   };
