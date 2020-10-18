67b30affc769d752c436a77f05246682bdb459df
Lluís Batlle i Rossell <viric@viric.name>
2020-10-18 22:30:03 +0200

    tribler: 7.5.0-rc5 -> 7.5.2
    Also make pyqtgraph usable in both qt4 and qt5
    
    (cherry picked from commit 2f7241232a69543aff9fdee83ff7b196e94bf4f4)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6406,7 +6406,13 @@ in {
     withWebKit = true;
   };
 
-  pyqtgraph = callPackage ../development/python-modules/pyqtgraph { };
+  pyqtgraph = callPackage ../development/python-modules/pyqtgraph {
+    pyqt = self.pyqt4;
+  };
+
+  pyqtgraph5 = callPackage ../development/python-modules/pyqtgraph {
+    pyqt = self.pyqt5;
+  };
 
   pyqtwebengine = pkgs.libsForQt5.callPackage ../development/python-modules/pyqtwebengine {
     pythonPackages = self;
@@ -7926,15 +7932,7 @@ in {
 
   sievelib = callPackage ../development/python-modules/sievelib { };
 
-<<<<<<< HEAD
   signedjson = callPackage ../development/python-modules/signedjson { };
-||||||| parent of 0499b8b5e16... Tribler 7.5.0-rc5
-  weasyprint = callPackage ../development/python-modules/weasyprint { };
-=======
-  webargs  = callPackage ../development/python-modules/webargs { };
-
-  weasyprint = callPackage ../development/python-modules/weasyprint { };
->>>>>>> 0499b8b5e16... Tribler 7.5.0-rc5
 
   sigtools = callPackage ../development/python-modules/sigtools { };
 
@@ -9127,6 +9125,8 @@ in {
   wcwidth = callPackage ../development/python-modules/wcwidth { };
 
   weasyprint = callPackage ../development/python-modules/weasyprint { };
+
+  webargs  = callPackage ../development/python-modules/webargs { };
 
   webapp2 = callPackage ../development/python-modules/webapp2 { };
 
