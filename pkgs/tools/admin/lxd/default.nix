396eb0c8466dd0dba6ade2d51565bb8e9c6a43b5
Maciej Krüger <mkg20001@gmail.com>
2021-04-16 21:24:21 +0200

    lxd: fix qemu

--- pkgs/tools/admin/lxd/default.nix
+++ pkgs/tools/admin/lxd/default.nix
@@ -7,6 +7,7 @@
 , bash
 , installShellFiles
 , nixosTests
+, OVMF, qemu_kvm, seabios, gptfdisk
 }:
 
 buildGoPackage rec {
@@ -23,6 +24,12 @@ buildGoPackage rec {
   postPatch = ''
     substituteInPlace shared/usbid/load.go \
       --replace "/usr/share/misc/usb.ids" "${hwdata}/share/hwdata/usb.ids"
+    # Nuke from orbit.
+    find . -type f -exec sed -i \
+      -e s,/usr/share/OVMF,${OVMF.fd}/FV,g \
+      -e s,/usr/share/qemu,${qemu_kvm}/share/qemu,g \
+      -e s,/usr/share/seabios,${seabios},g \
+      {} +
   '';
 
   preBuild = ''
@@ -38,7 +45,7 @@ buildGoPackage rec {
 
     wrapProgram $out/bin/lxd --prefix PATH : ${lib.makeBinPath (
       [ iptables ]
-      ++ [ acl rsync gnutar xz btrfs-progs gzip dnsmasq squashfsTools iproute2 bash criu attr ]
+      ++ [ acl rsync gnutar xz btrfs-progs gzip dnsmasq squashfsTools iproute2 bash criu attr qemu_kvm gptfdisk ]
       ++ [ (writeShellScriptBin "apparmor_parser" ''
              exec '${apparmor-parser}/bin/apparmor_parser' -I '${apparmor-profiles}/etc/apparmor.d' "$@"
            '') ]
@@ -52,7 +59,7 @@ buildGoPackage rec {
 
   nativeBuildInputs = [ installShellFiles pkg-config makeWrapper ];
   buildInputs = [ lxc acl libcap dqlite.dev raft-canonical.dev
-                  sqlite-replication udev.dev ];
+                  sqlite-replication udev.dev qemu_kvm ];
 
   meta = with lib; {
     description = "Daemon based on liblxc offering a REST API to manage containers";
