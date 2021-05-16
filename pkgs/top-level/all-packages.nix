c4a6a6925a4b80ec2bb5de94729d623a3bb510ee
Maciej Krüger <mkg20001@gmail.com>
2021-05-16 17:21:10 +0200

    qemu_5: re-add version 5 under prefix for LXD qemu support

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21519,6 +21519,7 @@ in
 
   prototool = callPackage ../development/tools/prototool { };
 
+  qemu_5_kvm = lowPrio (qemu_5.override { hostCpuOnly = true; });
   qemu_kvm = lowPrio (qemu.override { hostCpuOnly = true; });
   qemu_full = lowPrio (qemu.override { smbdSupport = true; cephSupport = true; glusterfsSupport = true; });
 
@@ -26387,6 +26388,12 @@ in
   qdirstat = libsForQt5.callPackage ../applications/misc/qdirstat {};
 
   qemu = callPackage ../applications/virtualization/qemu {
+    inherit (darwin.apple_sdk.frameworks) CoreServices Cocoa Hypervisor;
+    inherit (darwin.stubs) rez setfile;
+    python = python3;
+  };
+
+  qemu_5 = callPackage ../applications/virtualization/qemu-5 {
     inherit (darwin.apple_sdk.frameworks) CoreServices Cocoa Hypervisor;
     inherit (darwin.stubs) rez setfile;
     python = python3;
