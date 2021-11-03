5fa0d0ce2dd5def91ce5e44c5c392fd4059d1579
Maciej Krüger <mkg20001@gmail.com>
2021-11-03 05:32:02 +0100

    tribler: fixup

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -527,8 +527,6 @@ in {
 
   apispec = callPackage ../development/python-modules/apispec { };
 
-  apispec3 = callPackage ../development/python-modules/apispec/3.nix { };
-
   aplpy = callPackage ../development/python-modules/aplpy { };
 
   appdirs = callPackage ../development/python-modules/appdirs { };
@@ -9828,8 +9826,6 @@ in {
   weasyprint = callPackage ../development/python-modules/weasyprint { };
 
   webargs = callPackage ../development/python-modules/webargs { };
-
-  webargs5 = callPackage ../development/python-modules/webargs/5.nix { };
 
   webapp2 = callPackage ../development/python-modules/webapp2 { };
 
