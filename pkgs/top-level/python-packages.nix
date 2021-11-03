0ebac5bc3071fc77083efabdc1276d81a46b5cea
Maciej Krüger <mkg20001@gmail.com>
2021-11-03 05:03:22 +0100

    tribler: 7.4.4 -> 7.10.0
    This also adds old versions of apispec and webargs that tribler
    requires, and aiohttp-apispec as well.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -297,6 +297,8 @@ in {
     pytestCheckHook = self.pytestCheckHook_6_1;
   };
 
+  aiohttp-apispec = callPackage ../development/python-modules/aiohttp-apispec { };
+
   aiohttp-cors = callPackage ../development/python-modules/aiohttp-cors { };
 
   aiohttp-jinja2 = callPackage ../development/python-modules/aiohttp-jinja2 { };
@@ -522,6 +524,8 @@ in {
   apipkg = callPackage ../development/python-modules/apipkg { };
 
   apispec = callPackage ../development/python-modules/apispec { };
+
+  apispec3 = callPackage ../development/python-modules/apispec/3.nix { };
 
   aplpy = callPackage ../development/python-modules/aplpy { };
 
@@ -9788,6 +9792,10 @@ in {
 
   weasyprint = callPackage ../development/python-modules/weasyprint { };
 
+  webargs = callPackage ../development/python-modules/webargs { };
+
+  webargs5 = callPackage ../development/python-modules/webargs/5.nix { };
+
   webapp2 = callPackage ../development/python-modules/webapp2 { };
 
   webassets = callPackage ../development/python-modules/webassets { };
@@ -9805,8 +9813,6 @@ in {
   webexteamssdk = callPackage ../development/python-modules/webexteamssdk { };
 
   webhelpers = callPackage ../development/python-modules/webhelpers { };
-
-  webargs = callPackage ../development/python-modules/webargs { };
 
   webob = callPackage ../development/python-modules/webob { };
 
