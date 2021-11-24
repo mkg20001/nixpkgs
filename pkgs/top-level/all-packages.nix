986eacb83a38cccacb41c9f98bafdb3953b8a52e
Maciej Krüger <mkg20001@gmail.com>
2021-11-24 08:06:56 +0100

    cfs-zen-tweaks: init at 1.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4333,6 +4333,8 @@ with pkgs;
 
   cfssl = callPackage ../tools/security/cfssl { };
 
+  cfs-zen-tweaks = callPackage ../os-specific/linux/cfs-zen-tweaks { };
+
   chafa = callPackage ../tools/misc/chafa {
     inherit (darwin.apple_sdk.frameworks) Foundation;
   };
