2128a86d98d95a57c9cf84552311bc3cfee1186c
Maciej Krüger <mkg20001@gmail.com>
2021-11-24 08:07:08 +0100

    nixos/cfs-zen-tweaks: init
    Co-authored-by: Aaron Andersen <aaron@fosslib.net>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -132,6 +132,7 @@
   ./programs/captive-browser.nix
   ./programs/ccache.nix
   ./programs/cdemu.nix
+  ./programs/cfs-zen-tweaks.nix
   ./programs/chromium.nix
   ./programs/clickshare.nix
   ./programs/cnping.nix
