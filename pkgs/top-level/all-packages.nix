a484fd24196a13eae753f061e58ec34610aa2f2f
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2021-11-27 22:11:09 -0500

    mxnet: fix for gcc10+, enable on darwin

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32044,7 +32044,6 @@ with pkgs;
 
   mxnet = callPackage ../applications/science/math/mxnet {
     inherit (linuxPackages) nvidia_x11;
-    stdenv = gcc9Stdenv;
   };
 
   wxmaxima = callPackage ../applications/science/math/wxmaxima { wxGTK = wxGTK30; };
