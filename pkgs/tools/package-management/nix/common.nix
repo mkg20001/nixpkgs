f17b0d62f0785017c299a7923e0ea18edc88a50a
Maciej Krüger <mkg20001@gmail.com>
2021-11-30 06:48:10 +0100

    xeredoNix: init at xeredo-2.16-06.07.2023

--- pkgs/tools/package-management/nix/common.nix
+++ pkgs/tools/package-management/nix/common.nix
@@ -63,6 +63,7 @@ in
 , sqlite
 , util-linuxMinimal
 , xz
+, jemalloc
 
 , enableDocumentation ? !atLeast24 || stdenv.hostPlatform == stdenv.buildPlatform
 , enableStatic ? stdenv.hostPlatform.isStatic
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -40202,6 +40202,10 @@ with pkgs;
 
   nix = nixVersions.stable;
 
+  inherit (nixVersions)
+    nixFlakes
+    xeredoNix;
+
   nixStatic = pkgsStatic.nix;
 
   inherit (callPackages ../applications/networking/cluster/nixops { })
