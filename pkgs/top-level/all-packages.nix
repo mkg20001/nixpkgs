2dffb0ddb7e1765b3f3eff1b4845939138b0dcfb
Maciej Krüger <mkg20001@gmail.com>
2021-11-30 06:48:10 +0100

    xeredoNix: init at xeredo-2.12-11.12.2022

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -39115,6 +39115,10 @@ with pkgs;
 
   nix = nixVersions.stable;
 
+  inherit (nixVersions)
+    nixFlakes
+    xeredoNix;
+
   nixStatic = pkgsStatic.nix;
 
   nixops = callPackage ../tools/package-management/nixops { };
