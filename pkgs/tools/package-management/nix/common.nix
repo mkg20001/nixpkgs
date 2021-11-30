4b28f480c9770d044ce6b7f38a5a97704a7d5671
Maciej Krüger <mkg20001@gmail.com>
2021-11-30 06:48:10 +0100

    xeredoNix: init at xeredo-2.16-06.07.2023

--- pkgs/tools/package-management/nix/common.nix
+++ pkgs/tools/package-management/nix/common.nix
@@ -49,6 +49,7 @@ in
 , sqlite
 , util-linuxMinimal
 , xz
+, jemalloc
 
 , enableDocumentation ? !atLeast24 || stdenv.hostPlatform == stdenv.buildPlatform
 , enableStatic ? stdenv.hostPlatform.isStatic
@@ -103,6 +104,7 @@ self = stdenv.mkDerivation {
     openssl
     sqlite
     xz
+    jemalloc
   ] ++ lib.optionals stdenv.isDarwin [
     Security
   ] ++ lib.optionals atLeast24 [
