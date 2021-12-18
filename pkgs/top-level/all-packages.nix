873669cd78e08a0980f5f52ab866fc8b34aef27a
Mario Rodas <marsam@users.noreply.github.com>
2021-12-18 04:20:00 +0000

    ytarchive: init at 0.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29708,6 +29708,8 @@ with pkgs;
 
   ytalk = callPackage ../applications/networking/instant-messengers/ytalk { };
 
+  ytarchive = callPackage ../tools/misc/ytarchive { };
+
   ytcc = callPackage ../tools/networking/ytcc { };
 
   ytmdesktop = callPackage ../applications/audio/ytmdesktop { };
