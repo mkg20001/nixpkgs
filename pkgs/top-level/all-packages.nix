a17b412ba5f3c9ef96dfb23c125e4bb1dd1951f9
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2023-03-08 15:03:15 -0500

    sparrow: enable JavaFX
    See #206643

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12316,6 +12316,7 @@ with pkgs;
 
   sparrow-unwrapped = callPackage ../applications/blockchains/sparrow {
     openimajgrabber = callPackage ../applications/blockchains/sparrow/openimajgrabber.nix {};
+    openjdk = openjdk.override { enableJavaFX = true; };
   };
 
   sparrow = callPackage ../applications/blockchains/sparrow/fhsenv.nix {
