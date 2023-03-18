ff370ede66a8c74afa0d62ae2240f96ea6e00c87
COLAMAroro <github@rondier.io>
2023-03-18 16:34:10 +0100

    pulsar: init at 1.103.0
    Community maintained fork of the sunset Atom editor.
    https://github.com/pulsar-edit/pulsar

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28676,6 +28676,8 @@ with pkgs;
 
   inherit (atomPackages) atom atom-beta;
 
+  pulsar = callPackage ../applications/editors/pulsar { };
+
   asap = callPackage ../tools/audio/asap { };
 
   aseprite = callPackage ../applications/editors/aseprite { };
