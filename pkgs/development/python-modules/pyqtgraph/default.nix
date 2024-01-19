722844fd9bb61648af32431c0a5a295f0ba8ccaa
Maciej Krüger <mkg20001@gmail.com>
2024-01-19 02:26:51 +0100

    python3Packages.pyqtgraph: fix check

--- pkgs/development/python-modules/pyqtgraph/default.nix
+++ pkgs/development/python-modules/pyqtgraph/default.nix
@@ -62,6 +62,7 @@ buildPythonPackage rec {
     export QT_QPA_PLATFORM=offscreen
     export DYLD_FRAMEWORK_PATH=/System/Library/Frameworks
     export FONTCONFIG_FILE=${fontsConf}
+    export HOME=$(mktemp -d)
   '';
 
   enabledTestPaths = [
