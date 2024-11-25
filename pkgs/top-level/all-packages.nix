d6f894e552e592f443f2e8cbcc52d76ec710f882
Reno Dakota <paparodeo@proton.me>
2024-11-25 02:14:37 +0000

    xdp-tools: unpin LLVM-14
    > clang: error: unsupported option '-fzero-call-used-regs=used-gpr' for target 'bpf'
    
    required hardeningDisable zerocallusedregs

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5683,9 +5683,7 @@ with pkgs;
     buildGoModule = buildGo123Module;
   };
 
-  xdp-tools = callPackage ../tools/networking/xdp-tools {
-    llvmPackages = llvmPackages_14;
-  };
+  xdp-tools = callPackage ../tools/networking/xdp-tools { };
 
   ugarit = callPackage ../tools/backup/ugarit {
     inherit (chickenPackages_4) eggDerivation fetchegg;
