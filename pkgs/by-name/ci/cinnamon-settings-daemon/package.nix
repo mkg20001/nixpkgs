5b6c22230a7701ac8049151eb9cbb938608ead25
Bobby Rong <rjl931189261@126.com>
2024-11-28 21:24:46 +0800

    cinnamon-settings-daemon: 6.2.0 -> 6.4.0
    https://github.com/linuxmint/cinnamon-settings-daemon/compare/6.2.0...6.4.0

--- pkgs/by-name/ci/cinnamon-settings-daemon/package.nix
+++ pkgs/by-name/ci/cinnamon-settings-daemon/package.nix
@@ -32,13 +32,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cinnamon-settings-daemon";
-  version = "6.2.0";
+  version = "6.4.0";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-OAG5Tes+0bi+vKqm77Y7OykTpUkMdRaXIJYLuomIDMo=";
+    hash = "sha256-fVU1FLwhRQmHtpDATYg/g7s5KDgWa3woETD9yTm04F4=";
   };
 
   patches = [
