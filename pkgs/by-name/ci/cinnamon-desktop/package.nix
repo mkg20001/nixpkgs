42b78155de9c0f74a2c73e2b548d7b39455a52f3
Bobby Rong <rjl931189261@126.com>
2024-11-28 21:24:42 +0800

    cinnamon-desktop: 6.2.0 -> 6.4.0
    https://github.com/linuxmint/cinnamon-desktop/compare/6.2.0...6.4.0

--- pkgs/by-name/ci/cinnamon-desktop/package.nix
+++ pkgs/by-name/ci/cinnamon-desktop/package.nix
@@ -3,6 +3,7 @@
 , gobject-introspection
 , gtk3
 , intltool
+, isocodes
 , meson
 , ninja
 , pkg-config
@@ -19,13 +20,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cinnamon-desktop";
-  version = "6.2.0";
+  version = "6.4.0";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-9uewZh0GHQAenTcZpLchgFXSt3vOhxLbaepsJIkjTdI=";
+    hash = "sha256-QlBQTZIcmnMX+0ESLqdGXOgmYzE6WPJUTXq+wZVAq8Y=";
   };
 
   outputs = [ "out" "dev" ];
@@ -38,6 +39,7 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     gdk-pixbuf
+    isocodes
     systemd
     xkeyboard_config
     xorg.libxkbfile
