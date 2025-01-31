0a6748dd57002158e3142eeb7c88e3d393ee8654
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-01-31 09:23:39 +0100

    python312Packages.lib4package: init at 0.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7268,6 +7268,8 @@ self: super: with self; {
     inherit python;
   });
 
+  lib4package = callPackage ../development/python-modules/lib4package { };
+
   libagent = callPackage ../development/python-modules/libagent { };
 
   pa-ringbuffer = callPackage ../development/python-modules/pa-ringbuffer { };
