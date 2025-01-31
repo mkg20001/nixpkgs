ff1ead69eda92cd75d69dda967d2d678df8e74dc
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-01-31 09:34:39 +0100

    sbom4files: init at 0.4.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14522,6 +14522,8 @@ self: super: with self; {
 
   sbom2dot = callPackage ../development/python-modules/sbom2dot { };
 
+  sbom4files = callPackage ../development/python-modules/sbom4files { };
+
   scalene = callPackage ../development/python-modules/scalene { };
 
   scales = callPackage ../development/python-modules/scales { };
