3e2f1cf408610f07b934de0de90b27b28d95e30e
Florian Klink <flokli@flokli.de>
2025-03-25 20:09:38 +0000

    python312Packages.ipydatagrid: init at 1.4.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6815,6 +6815,8 @@ self: super: with self; {
 
   ipycanvas = callPackage ../development/python-modules/ipycanvas { };
 
+  ipydatagrid = callPackage ../development/python-modules/ipydatagrid { };
+
   ipydatawidgets = callPackage ../development/python-modules/ipydatawidgets { };
 
   ipykernel = callPackage ../development/python-modules/ipykernel { };
