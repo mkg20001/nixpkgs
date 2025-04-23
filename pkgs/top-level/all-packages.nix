7422a89fec178a24e7eebbf48c094af9f6bc2186
Zexin Yuan <git@yzx9.xyz>
2025-04-23 20:46:54 +0800

    openimageio: 2.5.17.0 -> 3.0.5.0; openimageio_2: init at 2.5.17.0
    Openimageio v3 was released in Nov 9, 2024. However, v3 introduces many
    breaking changes that some consumers are not yet ready for, and
    updating them would require complex changes. To address this, we’ve
    introduced openimageio_2 at version 2.5.17.0. This legacy package is
    expected to be removed once it is no longer used in nixpkgs.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15487,6 +15487,8 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) AppKit;
   };
 
+  openimageio_2 = callPackage ../by-name/op/openimageio/2.nix { };
+
   open-music-kontrollers = lib.recurseIntoAttrs {
     eteroj = callPackage ../applications/audio/open-music-kontrollers/eteroj.nix { };
     jit = callPackage ../applications/audio/open-music-kontrollers/jit.nix { };
