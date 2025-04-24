870b6bbd52171667bf675b16f7d8a3325392eb1e
DataHearth <dev@antoine-langlois.net>
2025-04-24 15:13:20 +0200

    python3Packages.scenedetect: init at 0.6.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15416,6 +15416,8 @@ self: super: with self; {
     inherit (pkgs) libpcap; # Avoid confusion with python package of the same name
   };
 
+  scenedetect = callPackage ../development/python-modules/scenedetect { };
+
   schedule = callPackage ../development/python-modules/schedule { };
 
   schema = callPackage ../development/python-modules/schema { };
