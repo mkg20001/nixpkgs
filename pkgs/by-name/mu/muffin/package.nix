26d6547e503d7e3f61259f7821fd762ea5398fe6
Maciej Krüger <mkg20001@gmail.com>
2025-09-23 16:38:33 +0200

    mu

--- pkgs/by-name/mu/muffin/package.nix
+++ pkgs/by-name/mu/muffin/package.nix
@@ -44,7 +44,7 @@
 
 stdenv.mkDerivation rec {
   pname = "muffin";
-  version = "6.4.1";
+  version = "add0f97604b87cc3e0091224c41677c131e70a61";
 
   outputs = [
     "out"
@@ -56,20 +56,13 @@ stdenv.mkDerivation rec {
     owner = "linuxmint";
     repo = "muffin";
     rev = version;
-    hash = "sha256-cGC1yGft3uEqefm2DvZrMaROoZKYd6LNY0IJ+58f6vs=";
+    hash = "sha256-3bGolRUcX/iLHXo0PeXI1qgQfigtR8/MEXxJcD8DfU0=";
   };
 
   patches = [
     ./keymap.patch
     (replaceVars ./fix-paths.patch {
       inherit zenity;
-    })
-
-    # Fix Qt apps crashing on wayland
-    # https://github.com/linuxmint/muffin/pull/739
-    (fetchpatch {
-      url = "https://github.com/linuxmint/muffin/commit/760e2a3046e13610c4fda1291a9a28e589d2bd93.patch";
-      hash = "sha256-D0u8UxW5USzMW9KlP3Y4XCWxrQ1ySufDv+eCbrAP71c=";
     })
   ];
 
