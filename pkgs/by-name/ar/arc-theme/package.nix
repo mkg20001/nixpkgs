ac78a45d9aa9b7a4274f96d7329c07fcf589dc5e
Maciej Krüger <mkg20001@gmail.com>
2025-09-23 16:56:46 +0200

    arc-theme: fixed version

--- pkgs/by-name/ar/arc-theme/package.nix
+++ pkgs/by-name/ar/arc-theme/package.nix
@@ -55,7 +55,7 @@ stdenv.mkDerivation rec {
   mesonFlags = [
     # "-Dthemes=cinnamon,gnome-shell,gtk2,gtk3,plank,xfwm,metacity"
     # "-Dvariants=light,darker,dark,lighter"
-    "-Dcinnamon_version=${cinnamon.version}"
+    "-Dcinnamon_version=6.4.13"
     "-Dgnome_shell_version=${gnome-shell.version}"
     # You will need to patch gdm to make use of this.
     "-Dgnome_shell_gresource=true"
