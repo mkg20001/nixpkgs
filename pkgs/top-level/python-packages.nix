9581196c1e50d49a3f973c98cf98e764cdc2718b
Pavol Rusnak <pavol@rusnak.io>
2025-10-28 19:01:30 +0100

    python3Packages.pyobjc-framework-libdispatch: init at 11.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13956,6 +13956,10 @@ self: super: with self; {
 
   pyobjc-framework-WebKit = callPackage ../development/python-modules/pyobjc-framework-WebKit { };
 
+  pyobjc-framework-libdispatch =
+    callPackage ../development/python-modules/pyobjc-framework-libdispatch
+      { };
+
   pyocd = callPackage ../development/python-modules/pyocd { };
 
   pyocd-pemicro = callPackage ../development/python-modules/pyocd-pemicro { };
