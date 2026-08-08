a18f493ca49080121af0a92f966bb461354000a3
K900 <me@0upti.me>
2026-08-08 18:36:46 +0300

    Revert "nixos/seerr: use lib.mkStateRevisionOption"

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1059,7 +1059,6 @@ in
   modularService = pkgs.callPackage ../modules/system/service/systemd/test.nix {
     inherit evalSystem;
   };
-  moduleStateRevisions = pkgs.callPackage ./moduleStateRevisions.nix { };
   molly-brown = runTest ./molly-brown.nix;
   mollysocket = runTest ./mollysocket.nix;
   monado = runTest ./monado.nix;
@@ -1841,7 +1840,7 @@ in
   userborn-mutable-users = runTest ./userborn-mutable-users.nix;
   userborn-static = runTest ./userborn-static.nix;
   ustreamer = runTest ./ustreamer.nix;
-  utils = pkgs.callPackage ./utils { inherit runTest; };
+  utils = import ./utils { inherit runTest; };
   uwsgi = runTest ./uwsgi.nix;
   v2ray = runTest ./v2ray.nix;
   varnish80 = runTest {
